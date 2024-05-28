c2d4d1ff54381cfcff90cff97283af85ee273be0
Fedotov <sasha4936@yandex.ru>
2024-05-28 23:50:10 +0500

    First Commit

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Click me") {
+                
+            }
+            Text("Subscribe me")
+            Text("Subscribe me")
+            Rectangle()
         }
         .padding()
     }
---
+++ SourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControl
+//
+//  Created by Fedotov Aleksandr on 28.05.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello Aleksandr")
+    }
+}
+
+#Preview {
+    HomeView()
+}
