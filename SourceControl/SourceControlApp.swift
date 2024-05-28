c402c932a8e9cfe7f05f9ab3f0cb03f8f3f3f2fa
Fedotov <sasha4936@yandex.ru>
2024-05-28 23:48:03 +0500

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Fedotov Aleksandr on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
