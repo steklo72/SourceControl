e15f83b436e4da1ed09264b25cab66210ec0a1d2
Fedotov <sasha4936@yandex.ru>
2024-05-30 23:43:02 +0500

    [Feature] bolt icon

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image(systemName: "bolt.fill")
             Text("Hello, world!")
             Button("Click me") {
                 
