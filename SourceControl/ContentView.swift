c0545b85c723d1ad592660f3cb78c90b1fff2482
Fedotov <sasha4936@yandex.ru>
2024-05-30 23:39:03 +0500

    [Feature] Change ocain to house

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image(systemName: "house.fill")
             Text("Hello, world!")
             Button("Click me") {
                 
